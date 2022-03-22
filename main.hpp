ede2e58173abb49cb3046b9fb69ef10fdf74e3d9
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2022-03-21 20:29:04 -0400

    oops forgot to track these

---
+++ main.hpp
@@ -0,0 +1,38 @@
+#ifndef MAIN_HPP_INCLUDED
+#define MAIN_HPP_INCLUDED
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#include <cmath>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+#define BUFFER_SIZE 1024
+
+// Function prototypes & namespaces
+namespace WordleEncryption {
+	void UpdateWindow(bool *open);
+
+	int EncryptFilter(ImGuiInputTextCallbackData* data);
+	void Encrypt(char in[], char out[]);
+	void Decrypt(char in[], char out[]);
+
+	void UpdateInfoWindow(bool *open);
+}
+
+namespace Utils {
+	void ClearBuffer(char buf[]);
+}
+
+namespace Origins {
+	void UpdateWindow(bool *open);
+	void UpdateLoadWindow(bool *open);
+}
+
+#endif//MAIN_HPP_INCLUDED
+
