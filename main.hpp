3759767dd8a447b38da40bf044e36e6aa302c587
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2022-04-08 15:47:46 -0400

    ignore

--- main.hpp
+++ main.hpp
@@ -17,10 +17,10 @@
 #include <cmath>
 #include <list>
 
-#include "imgui/imgui.h"
-#include "imgui/backends/imgui_impl_glfw.h"
-#include "imgui/backends/imgui_impl_opengl3.h"
-#include "imgui/imgui_internal.h"
+#include "../imgui/imgui.h"
+#include "../imgui/backends/imgui_impl_glfw.h"
+#include "../imgui/backends/imgui_impl_opengl3.h"
+#include "../imgui/imgui_internal.h"
 
 #define BUFFER_SIZE 1024
 
