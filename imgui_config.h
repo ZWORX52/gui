710e235d0839275c6d9839cc7a4f96f980cc5b1d
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2022-04-08 20:10:42 -0400

    now we can use the default imgui cloned repo. easier to install!

---
+++ imgui_config.h
@@ -0,0 +1,6 @@
+// Custom configuration header file for Dear ImGui. So I don't have to use imconfig.h!
+#define IMGUI_DEFINE_MATH_OPERATORS
+
+#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+#define IMGUI_DISABLE_OBSOLETE_KEYIO
+
